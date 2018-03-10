68eb9e20ed3d68fa0763d5ad58164c40960de45b
Jakub Man <jakubman1@gmail.com>
2018-03-10 13:15:12 +0100

    Add functions for basic operations to MathLibrary
    Add, Substract, Multiply and Divide

--- src/lib/MathLibrary/Class1.cs
+++ src/lib/MathLibrary/Class1.cs
@@ -8,14 +8,49 @@ namespace MathLibrary
 {
     public class Math
     {
+        /// <summary>
+        /// Adds two numbers
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Result of a + b</returns>
         public double Add(double a, double b)
         {
             return a + b;
         }
-
+        /// <summary>
+        /// Substracts b from a
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Result of a - b</returns>
         public double Sub(double a, double b)
         {
             return a - b;
         }
+        
+        /// <summary>
+        /// Multiplies two numbers
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Result of a * b</returns>
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// Divides two numbers
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Result of a / b</returns>
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+
     }
 }
