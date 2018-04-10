095690e0004bdc48ac602db2ff961d54fafe76d5
jakubman1 <jakubman1@gmail.com>
2018-04-10 16:59:28 +0200

    Add basic functionality for syntax highlighting

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Calculator
 {
@@ -24,5 +25,69 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private bool ready = true;
+        /// <summary>
+        /// When input text is changed, change colors of text based on syntax
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void InputTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(ready)
+            {
+                CreateColorSyntax(inputTextBox);
+            }
+             
+        }
+
+        private void CreateColorSyntax(RichTextBox textBox)
+        {
+            ready = false;
+            //Setup brushes
+            Brush numberBrush = new SolidColorBrush(Color.FromArgb(255, 34, 207, 247));
+            Brush operatorBrush = new SolidColorBrush(Color.FromArgb(255, 253, 115, 115));
+
+            //Store content
+            TextRange storedTextContent = new TextRange(textBox.Document.ContentStart, textBox.Document.ContentEnd);
+            string text = storedTextContent.Text;
+
+            if(text != "")
+            {
+       
+                //Remove content
+                inputTextBox.Document.Blocks.Clear();
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i] >= '0' && text[i] <= '9')
+                    {
+                        inputTextBox.Foreground = numberBrush;
+                        
+                    }
+                    else if(text[i] == '+' || text[i] == '=' || text[i] == '-' || text[i] == '*' || text[i] == '/')
+                    {
+                        inputTextBox.Foreground = operatorBrush;
+                    }
+                    else
+                    {
+                        inputTextBox.Foreground = Brushes.Red;
+                    }
+                    inputTextBox.AppendText(text[i].ToString());
+                }
+                
+
+                /*//Going through all numbers
+                for (Match m = r.Match(text); m.Success; m = m.NextMatch())
+                {
+                    textBox.Foreground = numberBrush;
+                    textBox.AppendText(m.Value);
+                }*/
+            }
+            ready = true;
+
+        }
+
+
     }
 }
