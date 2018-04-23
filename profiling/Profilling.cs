b1735b65134f4b271cd89fa3675b867f6bd31355
Šlesinger Josef (206780) <xslesi02@vutbr.cz>
2018-04-23 02:11:35 +0200

    Výběrová směrodatná odchylka
    Na 3 místech označených "Tu problem" je potřeba to provázat s mat. knihovnou.

---
+++ profiling/Profilling.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Profilling
+{
+    static void Main(string[] args)
+    {
+        string line;
+        int x = 0;
+        int x2;
+        double[] array;
+        double suma = 0;
+        double prum;
+
+        //cteni
+        while ((line = Console.ReadLine()) != null)
+        {
+            array[x] = line;
+            x++;
+        }
+
+        x2 = x;
+
+        //soucet vsech prvku pole
+        do while (x2 != 0)
+            {
+                suma = array[x2] + suma;
+                x2--;
+            }
+
+        //vypocet prumeru
+        prum = suma/x   //tu problem
+        x2 = x;
+
+        //vypocet jednotlivych prvku sumy
+        do while (x2 != 0)
+            {
+                suma = (array[x2] - prum)* (array[x2] - prum) + suma //tu problem
+                    x2--;
+            }
+
+        //vrati odchylku
+        return squareroot(suma/x-1)  //tu problem
+
+    }
+}
